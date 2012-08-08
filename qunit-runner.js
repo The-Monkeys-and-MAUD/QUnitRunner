bddf77f70269b7a0d3bf8884fe390fa19476582f
Kynan <id@mx.kynan.net.au>
2012-08-08 18:11:24 +1000

    Added support for the ui-tester assertion syntax etension

--- qunit-runner.js
+++ qunit-runner.js
@@ -3,6 +3,36 @@
 
 $('#qunit').css("visibility", "visible");
 
+// This adds support for the extension that ui-tester.js:
+// http://dsheiko.com/weblog/functional-testing-with-qunit/
+// adds to QUnit
+$.extend(window, {
+    testNodes : function (queue) {
+        var assertions =  {
+            exists: function () {
+                return this.length;
+            },
+            visible: function () {
+                return this.is(":visible");
+            },
+            hidden: function () {
+                return this.is(":hidden");
+            },
+            checked: function () {
+                return this.is(":checked");
+            }
+        };
+        $.each(queue, function (inx, request) {
+            var node = $(request.node);
+            //alert(request.assert);
+            //alert(request.msg);
+            //alert(assertions[request.assert].call(node));
+            ok(assertions[request.assert].call(node), request.msg);
+        });
+    }
+});
+
+
 eval($('.ui-tester').text());
 
 //alert($('.ui-tester'));
